f77dbc096c79264efc9af95c10439ec474e35e4f
Vadzim Zieńka <v@vadzim.info>
2019-12-28 11:25:58 +0300

    move src/symbolAsyncIterator.js -> src/polyfills/

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // @flow
 
-import "./symbolAsyncIterator.js"
+import "./polyfills/symbolAsyncIterator.js"
 
 // eslint-disable-next-line import/no-default-export
 export { merge as default } from "./mergeiterator.js"
